2336f6d3aa7bb72e4b23d8762220578a51a298fb
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-06-29 15:55:21 +0900

    [Finish]7章

---
+++ 7章/lesson6.rb
@@ -0,0 +1,23 @@
+# apple = 110
+# if apple >= 100
+# 	puts "りんごの値段は100円以上です"
+# end
+
+# if apple <= 100
+# 	puts "りんごの値段は100円以下です"
+# end
+
+# tall = 180
+# if tall >= 170 && tall <=190
+# 	puts "身長は170以上190以下です。"
+# end
+
+total_price = 1
+
+if total_price > 100
+	puts "みかんを購入。所持金に余りあり"
+elsif total_price == 100
+	puts "みかんを購入。所持金は0円"
+else
+	puts "みかんを購入することができません"
+end
