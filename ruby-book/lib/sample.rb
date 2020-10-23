553d13bac0b752f6bb16c9cce1cd9d348d59837f
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-10-23 16:10:47 +0900

    [Practice]Regexp checker

--- ruby-book/lib/sample.rb
+++ ruby-book/lib/sample.rb
@@ -1,16 +1,18 @@
-puts 'Start.'
-module Greeter
-  def hello
-    'hello'
+def currency_of(country)
+  case country
+  when :japan
+    'yen'
+  when :us
+      'dollar'
+    when :india
+      'rupee'
+    else
+      # raiseで意図的に例外を発生させる
+      raise "無効な国名です。#{country}"
   end
 end
 
-# 例外処理を組み込んで例外に対処する
-begin
-  greeter = Greeter.new
-rescue
-  puts '例外が発生したが、このまま続行する'
-end
-
-# 例外処理を組み込んだので、最後まで実行可能
-puts 'End.'
+puts currency_of(:japan)
+puts currency_of(:us)
+puts currency_of(:india)
+puts currency_of(:italy)
